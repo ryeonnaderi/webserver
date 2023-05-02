7205745fb9efb2bc4917c081ed87dc9d6eb2c779
Steven Compton <131310849+trap-stevo@users.noreply.github.com>
2023-05-02 14:25:58 -0400

    Update HypeUser.js

--- models/HypeUser.js
+++ models/HypeUser.js
@@ -3,21 +3,21 @@ const schema = mysql.schema;
 const Userschema = new schema({
 
   HypeID:{
-      type: Integer,
+      type: Number,
       required: " ID is required",
     },
     
     HypeProfilecontentID:{
-        type: Integer
+        type: Number
     },
     HypeCredentialID:{
-        type: Integer
+        type: Number
     },
     HypeIdentificationID:{
-        type: Integer
+        type: Number
     },
     HypeNameID:{
-        type: Integer
+        type: Number
     }
 
 
@@ -58,4 +58,4 @@ const Userschema = new schema({
     // },
     
 })
-const HypeUser = mysql.model("user", Userschema)
+const hypeUser = mysql.model("HypeUser", Userschema)
